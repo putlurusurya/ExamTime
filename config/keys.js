bfb85d213438ab090fd818b11f81268ba1a60b4a
putlurusurya <43723182+putlurusurya@users.noreply.github.com>
2020-10-29 16:28:56 +0530

    Add files via upload

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+dbPassword = 'mongodb+srv://YOUR_USERNAME_HERE:'+ encodeURIComponent('YOUR_PASSWORD_HERE') + '@CLUSTER_NAME_HERE.mongodb.net/test?retryWrites=true';
+
+module.exports = {
+    mongoURI: "mongodb+srv://fidistar:myBfucba6LQhchv@cluster0.ukdx3.mongodb.net/<dbname>?retryWrites=true&w=majority"
+};
